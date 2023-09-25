a5b1d7b6058e4d57ecfc19ac7d868def6328e41c
sssreddys <saragada566@gmail.com>
2023-09-25 10:39:47 +0530

    new changes

--- app/Livewire/TeachersEdit.php
+++ app/Livewire/TeachersEdit.php
@@ -10,83 +10,158 @@ use Livewire\WithFileUploads;
 
 class TeachersEdit extends Component
 {
-
     use WithFileUploads;
-    public $first_name,$last_name,$email, $image,$mobile, $teacher_edit_id, $address, $date_of_birth, $gender, $experience, $qualification, $remarks, $teacher;
-        
 
+ 
 
-    public function mount($id)
-    {
-        $this->teacher = Staff::find($id);
-        $this->first_name = $this->teacher->first_name;
-        $this->last_name = $this->teacher->last_name;
-        $this->email = $this->teacher->email;
-        $this->image = $this->teacher->image_path;
-        $this->mobile = $this->teacher->phone_no;
-        $this->address = $this->teacher->address;
-        $this->date_of_birth = $this->teacher->dob;
-        $this->gender = $this->teacher->gender;
-        $this->experience = $this->teacher->work_experience;
-        $this->qualification = $this->teacher->qualification;
-    }
+    public $name, $email, $mobile, $staff_edit_id, $address, $date_of_birth, $gender, $experience, $qualification, $remarks, $staff;
 
+    public $tempImage = null;
+
+    public $image = null;
    
-    public function fileChoosen($image)
-    {
-        $this->emit('fileChoosen', $this->image);
-    }
-    public function storeImage()
-    {
-        if (!$this->image) {
-            return null;
+
+    
+
+
+    
+    
+        public function mount($id)
+    
+        {
+    
+            $this->staff = Staff::find($id);
+    
+            $this->name = $this->staff->first_name;
+    
+            $this->email = $this->staff->email;
+    
+            $this->tempImage = $this->staff->image_path;
+    
+            $this->mobile = $this->staff->phone_no;
+    
+            $this->address = $this->staff->address;
+    
+            $this->date_of_birth = $this->staff->dob;
+    
+            $this->gender = $this->staff->gender;
+    
+            $this->experience = $this->staff->work_experience;
+    
+            $this->qualification = $this->staff->qualification;
+    
+     
+    
+            $this->staff_edit_id = $this->staff->staff_id;
+    
         }
-
-        $name = Str::random() . '.jpg';
-        $this->image->storeAs('public', $name);
-
-        return $name;
-    }
-
-    public function updateTeacher($id)
-    {
-        $this->validate([
-            'first_name' => 'required|string|max:255',
-            'email' => 'required|email|unique:teachers,email,' . $this->teacher_edit_id,
-            'image' => 'required|image|unique:teachers,image,' . $this->teacher_edit_id,
-            'mobile' => 'required|numeric|unique:teachers,mobile,' . $this->teacher_edit_id,
-            'address' => 'required|string|unique:teachers,address,' . $this->teacher_edit_id,
-            'date_of_birth' => 'required', // Correct date format
-            'gender' => 'required|string|unique:teachers,gender,' . $this->teacher_edit_id,
-            'experience' => 'required|string|unique:teachers,experience,' . $this->teacher_edit_id,
-            'qualification' => 'required|string|unique:teachers,qualification,' . $this->teacher_edit_id,
-            //'remarks' => 'required|string|unique:teachers,remarks,' . $this->teacher_edit_id,
-        ]);
     
-        $teacher = Staff::find($id);
-        $teacher->name = $this->first_name;
-        $teacher->email = $this->email;
-        $teacher->mobile = $this->mobile;
-        $teacher->address = $this->address;
-        if ($this->image) {
-            $imageName = time() . '.' . $this->image->getClientOriginalExtension();
-            $this->image->storeAs('public/images', $imageName);
-            $teacher->image = 'images/' . $imageName;
+     
+    
+        public function updatedImage($value)
+    
+        {
+    
+            if ($value instanceof \Illuminate\Http\UploadedFile) {
+    
+                $this->validate([
+    
+                    'image' => 'image|max:2048', // Add validation rules as needed.
+    
+                ]);
+    
+       
+    
+                $this->tempImage = $value; // Assign the uploaded file to the $tempImage property.
+    
+            } elseif (!$this->tempImage && $this->image) {
+    
+                // If no new image uploaded and there is an existing image, display the existing image.
+    
+                $this->tempImage = $this->image;
+    
+            }
+    
         }
-        // Convert date_of_birth to 'Y-m-d' format
-        $teacher->date_of_birth =  $this->date_of_birth;
     
-        $teacher->gender = $this->gender;
-        $teacher->experience = $this->experience;
-        $teacher->qualification = $this->qualification;
-        $teacher->remarks = $this->remarks;
+       
     
-        $teacher->save();
+     
     
-        session()->flash('message', 'Teacher has been updated successfully');
-    }
-    public function render()
+        public function storeImage()
+    
     {
-        return view('livewire.teachers-edit');
+    
+        if ($this->tempImage instanceof \Illuminate\Http\UploadedFile) {
+    
+            // Store and return the path for the new image.
+    
+            $name = 'profile_'  . '.' . $this->tempImage->getClientOriginalExtension();
+    
+            $this->tempImage->storeAs('public', $name);
+    
+            return $name;
+    
+        }
+    
+     
+    
+        // If no new image is uploaded, return the existing image path.
+    
+        return $this->staff->image_path;
+    
     }
-}
+    
+     
+    
+    public function updateTeacher()
+    
+    {
+    
+        // Validate other form fields as needed.
+    
+     
+    
+        $staff = Staff::find($this->staff_edit_id);
+    
+     
+    
+        $staff->first_name = $this->name;
+    
+        $staff->email = $this->email;
+    
+        $staff->phone_no = $this->mobile;
+    
+        $staff->address = $this->address;
+    
+        $staff->image_path = $this->storeImage(); // Store the image and assign the path.
+    
+        $staff->dob = $this->date_of_birth;
+    
+        $staff->gender = $this->gender;
+    
+        $staff->work_experience = $this->experience;
+    
+        $staff->qualification = $this->qualification;
+    
+     
+    
+        $staff->save();
+    
+     
+    
+        session()->flash('message', 'Staff has been updated successfully');
+    
+    }
+    
+     
+    
+        public function render()
+    
+        {
+    
+            return view('livewire.teachers-edit');
+    
+        }
+    
+    } 
